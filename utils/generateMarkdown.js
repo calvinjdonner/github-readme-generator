e580244c1e1646653c28a09e95025cdd1b7f4848
Calvin Donner <calvinjdonner@gmail.com>
2021-09-26 21:38:35 -0600

    updated markdown readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -35,7 +35,7 @@ function generateMarkdown(answers) {
   
   ## License
   ![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
-  <br />
+  <br/>
   This application is covered by the ${answers.license} license. 
   
   ## Contributing
@@ -45,11 +45,11 @@ function generateMarkdown(answers) {
   📚 ${answers.tests}
   
   ## Questions
-  ❓ ${answers.questions}<br />
-  <br />
-   Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
-  <br />
-  📯 Email me with any questions: ${answers.email}<br /><br />
+  ❓ ${answers.questions}<br/>
+  <br/>
+  :octocat: Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br/>
+  <br/>
+  📯 Email me with any questions: ${answers.email}<br/>
       `;
     };
       
